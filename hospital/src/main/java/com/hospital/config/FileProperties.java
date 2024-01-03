f3cc375b5512ba3d986453445f9cd08800aafcf8
pdoha <thdus5147@naver.com>
2024-01-03 22:38:40 +0900

    초기 업로드

---
+++ hospital/src/main/java/com/hospital/config/FileProperties.java
@@ -0,0 +1,12 @@
+package com.hospital.config;
+
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties(prefix = "file.upload")
+public class FileProperties {
+    private String path;
+    private String url;
+}
