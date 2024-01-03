dbcf1dcc39b3602c322b299811ffcf5f0d0ba613
pdoha <thdus5147@naver.com>
2024-01-03 22:58:57 +0900

    초기설정

---
+++ hospital/src/main/java/com/hospital/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.hospital.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    /*public SecurityFilterChain filterChain*/
+}
